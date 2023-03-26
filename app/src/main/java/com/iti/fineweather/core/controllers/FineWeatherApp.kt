da5fd60270a16eb4bb559c29b9e8e436656908a7
kirito-kun <drsh_201122@yahoo.com>
2023-03-27 01:04:48 +0200

    initial commit

---
+++ app/src/main/java/com/iti/fineweather/core/controllers/FineWeatherApp.kt
@@ -0,0 +1,8 @@
+package com.iti.fineweather.core.controllers
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FineWeatherApp: Application() {
+}
